b56d75d26f2913137da242aec857025cd4e3509a
AnanasicLETI <anastacia290597@mail.ru>
2020-03-18 15:29:25 +0300

    Discovery: add DiscoveryService and create the docker container

--- repository-service/src/main/kotlin/leti/project/storage/StorageApplication.kt
+++ repository-service/src/main/kotlin/leti/project/storage/StorageApplication.kt
@@ -2,8 +2,10 @@ package leti.project.storage
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class StorageApplication
 
 fun main(args: Array<String>) {
